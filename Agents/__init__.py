f278cbb1941deb043bed0db91f1a456922684caa
samlerman <slerman@ur.rochester.edu>
2021-11-28 17:26:32 -0500

    debug and run

--- Agents/__init__.py
+++ Agents/__init__.py
@@ -2,9 +2,4 @@
 #
 # This source code is licensed under the MIT license found in the
 # MIT_LICENSE file in the root directory of this source tree.
-# from .DrQV2 import DrQV2Agent
-from .DrQ_DQN import DrQV2Agent
-from .SAC import SACAgent
-from .Lermanbots.rQdia import rQdiaAgent
-from .Lermanbots.BVS import BVSAgent
-from .Lermanbots.PRO import PROAgent
+from .DQNDPG import DQNDPGAgent
