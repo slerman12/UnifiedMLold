5ee9ae6f7af05a812731333b921b2bbebb4af4a4
samlerman <slerman@ur.rochester.edu>
2021-11-28 17:37:48 -0500

    debug and run

--- Datasets/Environments/Raw/__init__.py
+++ Datasets/Environments/Raw/__init__.py
@@ -1,14 +1,14 @@
-import AtariRecommended
-import AtariRecommended as Atari
-import AtariRecommended as AtariALE
-import AtariRecommended as AtariArcade
-import AtariRecommended as AtariArcadeLearning
-import AtariRecommended as AtariArcadeLearningEnvs
-import AtariRecommended as AtariArcadeLearningEnvironments
-import AtariRecommended as AtariRevisited
-import DMC
-import DMC as DMCSuite
-import DMC as DMControl
-import DMC as DMControlSuite
-import DMC as DeepMindControl
-import DMC as DeepMindControlSuite
+from . import AtariRecommended
+from . import AtariRecommended as Atari
+from . import AtariRecommended as AtariALE
+from . import AtariRecommended as AtariArcade
+from . import AtariRecommended as AtariArcadeLearning
+from . import AtariRecommended as AtariArcadeLearningEnvs
+from . import AtariRecommended as AtariArcadeLearningEnvironments
+from . import AtariRecommended as AtariRevisited
+from . import DMC
+from . import DMC as DMCSuite
+from . import DMC as DMControl
+from . import DMC as DMControlSuite
+from . import DMC as DeepMindControl
+from . import DMC as DeepMindControlSuite
