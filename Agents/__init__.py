cb9d56bfec28ec9cb2b10a2f68d97ed253b7f163
samlerman <slerman@ur.rochester.edu>
2021-11-28 17:30:36 -0500

    debug and run

--- Agents/__init__.py
+++ Agents/__init__.py
@@ -2,4 +2,4 @@
 #
 # This source code is licensed under the MIT license found in the
 # MIT_LICENSE file in the root directory of this source tree.
-from Agents.DQNDPG import DQNDPGAgent
+from .DQNDPG import DQNDPGAgent
