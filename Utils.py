d021c6206fec0e1fad7b62146ea2747d18ffd24a
samlerman <slerman@ur.rochester.edu>
2021-11-29 00:41:31 -0500

    debug and run

--- Utils.py
+++ Utils.py
@@ -49,6 +49,7 @@ def optimize(*models):
                 # Update models
                 loss.backward()
                 if step_optim:
+                    print("yayaya")
                     map(lambda model: getattr(self, model).optim.step(), models)
 
         return model_loss
