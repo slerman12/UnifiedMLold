6c814fc30a1d486576d21c0b72479108f37a1a86
samlerman <slerman@ur.rochester.edu>
2021-12-01 10:32:03 -0500

    final base?

--- Agents/__init__.py
+++ Agents/__init__.py
@@ -2,3 +2,5 @@
 #
 # This source code is licensed under the MIT license found in the
 # MIT_LICENSE file in the root directory of this source tree.
+from Agents.DQNDPG import DQNDPGAgent
+from Agents.DrQV2 import DrQV2Agent
