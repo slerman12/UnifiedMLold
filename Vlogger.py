19edbe81994dad94959efd4e124fc459a55a301e
samlerman <slerman@ur.rochester.edu>
2021-12-01 19:16:05 -0500

    vlogger

--- Vlogger.py
+++ Vlogger.py
@@ -2,12 +2,13 @@
 #
 # This source code is licensed under the MIT license found in the
 # MIT_LICENSE file in the root directory of this source tree.
+from pathlib import Path
 import imageio  # M1 Mac: comment out freeimage imports in imageio/plugins/_init_
 
 
 class Vlogger:
     def __init__(self, root_path, fps):
-        self.save_path = root_path / 'Video'
+        self.save_path = Path(root_path) / 'Video'
         self.save_path.mkdir(exist_ok=True)
         self.fps = fps
 
