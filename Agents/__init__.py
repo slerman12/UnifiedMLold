71eaeaadb409a1a40f0aa0a523829075d7a4aad6
samlerman <slerman@ur.rochester.edu>
2021-12-03 18:07:13 -0500

    SPR! (?)

--- Agents/__init__.py
+++ Agents/__init__.py
@@ -5,4 +5,4 @@
 from Agents.DQNDPG import DQNDPGAgent
 from Agents.DrQV2 import DrQV2Agent
 from Agents.Lermanbots.BVS import BVSAgent
-from Agents.Lermanbots.SPR import SPRAgent
+from Agents.SPR import SPRAgent
