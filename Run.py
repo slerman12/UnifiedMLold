42749e663a4ff12221788e490570bd64fca580a6
samlerman <slerman@ur.rochester.edu>
2021-12-10 13:26:35 -0500

    debug

--- Run.py
+++ Run.py
@@ -58,6 +58,7 @@ def reinforce(args, root_path):
             setattr(args, arg, getattr(env, arg))
 
         # Agent
+        print(args.agent)
         agent = instantiate(args.agent)  # An instance of DQNDPGAgent, for example
 
         # Experience replay
