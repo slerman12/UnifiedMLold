6a928e98572c5647f9c70fe535bd4c12f271eed5
samlerman <slerman@ur.rochester.edu>
2021-12-13 11:27:44 -0500

    testing AGI

--- Blocks/Architectures/Lermanblocks/AGIGradient.py
+++ Blocks/Architectures/Lermanblocks/AGIGradient.py
@@ -158,7 +158,7 @@ class AGIGradient(nn.Module):
             mem_size = self.memories[ith][0].shape[1]
 
             if sense_size < mem_size:
-                self.memories[ith] = tuple(m[:, :sense_size] for m in self.memories[ith])
+                self.memories[ith] = tuple(m[:, :sense_size].contiguous() for m in self.memories[ith])
             elif sense_size > mem_size:
                 self.memories[ith] = tuple(m.repeat(1, sense_size // mem_size, 1).contiguous() for m in self.memories[ith])
                 nulls = self.null_memory.repeat(1, sense_size % mem_size, 1).contiguous()
