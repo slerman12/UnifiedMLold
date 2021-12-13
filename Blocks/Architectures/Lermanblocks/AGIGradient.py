8d6c605c080d891a1322dcfc444a029f8919bd26
samlerman <slerman@ur.rochester.edu>
2021-12-13 17:59:27 -0500

    testing AGI

--- Blocks/Architectures/Lermanblocks/AGIGradient.py
+++ Blocks/Architectures/Lermanblocks/AGIGradient.py
@@ -180,8 +180,11 @@ class AGIGradient(nn.Module):
     def forward(self, sense, label=torch.empty(0)):
         # TODO if label, do batch one at a time
         assert isinstance(sense, torch.Tensor) and isinstance(label, torch.Tensor)
-        return torch.stack([self.AGI((sense[i],), (label[i],) if len(label) > 0 else None)[0]
-                            for i in range(sense.shape[0])])
+        transmit = torch.stack([self.AGI((sense[i].unsqueeze(0),),
+                                         (label[i].unsqueeze(0),) if len(label) > 0 else None)[0]
+                                for i in range(sense.shape[0])])
+        self.memories = self.memories_detach()
+        return transmit
 
     def memories_detach(self):
         return [tuple(m.detach() for m in mem) for mem in self.memories]
